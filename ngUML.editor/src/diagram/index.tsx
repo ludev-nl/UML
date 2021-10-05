148763d33eb419c78f6d99efed175d7995e7f62e
Job van Dijke <jobvdijke@gmail.com>
2021-10-05 14:56:07 +0200

    name change of editor folder

---
+++ ngUML.editor/src/diagram/index.tsx
@@ -0,0 +1,13 @@
+import EnumNode from './EnumNode'
+import ClassNode from './ClassNode'
+import InitialNode from './InitialNode'
+import ActionNode from './ActionNode'
+import DecisionNode from './DecisionNode'
+
+export {
+    EnumNode,
+    ClassNode,
+    InitialNode,
+    ActionNode,
+    DecisionNode
+}
