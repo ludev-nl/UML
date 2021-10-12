b1dea23f13f72942599e594622729105c822c4c1
Samuel Smulders <samuel.smulders@hotmail.com>
2021-10-12 15:20:31 +0200

    header

--- ngUML.editor/src/shell/RuleMenu/index.tsx
+++ ngUML.editor/src/shell/RuleMenu/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './_rulemenu.css'
 
+		
 export const RuleMenu: React.FC = () => {
     return (
         <div
             id="RuleMenu"
         >
-            fds
+        	<span 
+        		id="Header"
+        	>
+        		Rule menu 
+        	</span>
         </div>
     )
 }
