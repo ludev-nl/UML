b07af223549af42922847ff046c9301a0ef5c4e9
Job van Dijke <jobvdijke@gmail.com>
2021-10-17 19:35:00 +0200

    missed some files

--- ngUML.editor/src/shell/RuleMenu/index.tsx
+++ ngUML.editor/src/shell/RuleMenu/index.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './_rulemenu.css'
+import {
+    TextArea,
+    Button,
+    OrderedList,
+    ListItem
+} from 'carbon-components-react'
 
 		
 export const RuleMenu: React.FC = () => {
+    const [rulesState, setRulesState] = useState<JSX.Element[]>([])
+    const [rulesStringState, setRulesStringState] = useState<string[]>([])
+
+    function apiToRules() {
+        var apiRules: string[] = ["Rule een", "Rule twee"]
+        var rules = rulesStringState
+        for (let apiRule of apiRules) {
+            rules.push(apiRule)
+        }
+        setRulesStringState(rules)
+        rulesToComponents()
+    }
+
+    function addToRules(toAdd: string) {
+        var rules = rulesStringState
+        rules.push(toAdd)
+        setRulesStringState(rules)
+        rulesToComponents()
+    }
+
+    function rulesToComponents() {
+        var rulesComponents: JSX.Element[] = []
+        for (let rule of rulesStringState) {
+            let ruleComponent: JSX.Element = <ListItem key={rule}>{rule}</ListItem>
+            rulesComponents.push(ruleComponent)
+        }
+        setRulesState(rulesComponents)
+    }
+
+
     return (
         <div
             id="RuleMenu"
@@ -10,8 +46,51 @@ export const RuleMenu: React.FC = () => {
         	<span 
         		id="Header"
         	>
-        		Rule menu 
-        	</span>
+        		Rules
+            </span>
+            <TextArea
+                labelText=""
+                placeholder="Write here your rule..."
+                id="RuleTextArea"
+            >
+            </TextArea>
+            <Button
+                id="AddRuleButton"
+                onClick={() => {
+                    let textAreaObject = document.getElementById("RuleTextArea") as HTMLInputElement
+                    let valueOfTextArea = textAreaObject.value
+                    let badrule = document.getElementById("BadRules")
+                    if (valueOfTextArea.includes("bad")) {
+                        badrule?.classList.add("BadRulesShown")
+                    }
+                    else {
+                        addToRules(valueOfTextArea)
+                        badrule?.classList.remove("BadRulesShown")
+                    }
+                    console.log(valueOfTextArea)
+                }}
+            >
+                Add rule
+            </Button>
+            <span
+                id="BadRules"
+            >
+                You entered a bad rule!
+            </span>
+            <hr>
+            </hr>
+            <Button
+                onClick={() => {
+                    apiToRules()
+                }}
+            >
+                Get rules from database
+            </Button>
+            <OrderedList
+                id="ruleList"
+            >
+                {rulesState}
+            </OrderedList>
         </div>
     )
 }
