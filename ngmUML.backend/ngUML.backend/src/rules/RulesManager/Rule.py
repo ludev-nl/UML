16cb7fe5deec78bdd5d110b3cddd5467bd6c957d
rlaanen <78648568+rlaanen@users.noreply.github.com>
2021-10-17 22:27:55 +0200

    check syntax of string for simple rule.

--- ngmUML.backend/ngUML.backend/src/rules/RulesManager/Rule.py
+++ ngmUML.backend/ngUML.backend/src/rules/RulesManager/Rule.py
@@ -1,33 +1,42 @@
 from ..processors.TextProcessor import split_rule
 
 class Rule:
-    def __init__(self, textrule):
+    def __init__(self, text_rule):
+
+        
         ''' Constructor function'''
         # Rule for now is a string with a strict structure: 
         # "<classifier> <attribute> <condition> <value>"
 
         # Example:
         # "Warehouse storagecapacity atleast 50"
+        self.text_rule = text_rule
+        #rule = split_rule(textrule) # Process text (for now just a split function on ' ') (Just to showcase that the text processor can be used and expanded)
+        #self.classifier = rule[0] 
+        #self.attribute = rule[1]
+        
+        #self.condition = rule[2]
+        #self.value = rule[3]
 
-        rule = split_rule(textrule) # Process text (for now just a split function on ' ') (Just to showcase that the text processor can be used and expanded)
-        self.classifier = rule[0] 
-        self.attribute = rule[1]
-        self.condition = rule[2]
-        self.value = rule[3]
+    def check_rule(self):
+        valid = False
+        valid = split_rule(self.text_rule)
+        return valid
+
 
     def get_as_dict(self):
         ''' Returns the rule as a dictionary (JSON) with all individual properties as keys.'''
-
+        rule = self.text_rule.lower().split()
         return {
-            'classifier': self.classifier,
-            'attribute': self.attribute,
-            'condition': self.condition,
-            'value': self.value
+            'classifier': rule[0],
+            'attribute': rule[1],
+            'condition': rule[2],
+            'value': rule[3]
         }
 
     def get_as_text(self):
         ''' Returns the entire rule as text '''
-        return self.classifier + " " +  self.attribute + " " +  self.condition + " " + self.value
+        return self.text_rule
 
 
         
--- ngmUML.backend/ngUML.backend/src/rules/processors/TextProcessor.py
+++ ngmUML.backend/ngUML.backend/src/rules/processors/TextProcessor.py
@@ -3,4 +3,30 @@
 def split_rule(text):
     ''' Splits a string into individual words, just to showcase that the textprocessor is 
     now integrated and accessible for the rest of the project'''
-    return text.split(' ')
+
+     # Example:
+        # "Warehouse storagecapacity atleast 50"
+    #uncapatilize all words. 
+    classifier = ["warehouse"]
+    attribute = ["storagecapacity"]
+    condition = ["atleast", "atmost"] 
+    
+    verify_components = text.lower().split()
+
+    if( verify_components[0] not in classifier):
+        print("error no classifier")
+        return False
+    
+    if(verify_components[1] not in attribute):
+        print("error no attribute")
+        return False
+
+    if(verify_components[2] not in condition):
+        print("error no condition")
+        return False
+
+    if(not verify_components[3].isdecimal()):
+        print("Error no numeric value")
+        return False
+    
+    return True
