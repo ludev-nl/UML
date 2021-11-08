11bad681d474bd8e6920bf322cac78a67effb003
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2021-11-08 16:37:19 +0100

    Added property and classifier getter from database

--- ngmUML.backend/ngUML.backend/src/rules/processors/MappingProcessor.py
+++ ngmUML.backend/ngUML.backend/src/rules/processors/MappingProcessor.py
@@ -1,1 +1,16 @@
 #Class that can tell if an instance of a Rule has correct references to objects in UML database
+from model.models import Classifier, Property
+
+def getClassifiers():
+    names = list()
+    for classifier in Classifier.objects.all():
+        names.append(classifier.name)
+
+    return names
+
+def getProperties():
+    names = list()
+    for property in Property.objects.all():
+        names.append(property.name)
+
+    return names
