37adec43ac731f7a77f3bc233b37813153b36208
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2021-11-08 17:05:14 +0100

    Added comments and classfier specific properties get function

--- ngmUML.backend/ngUML.backend/src/rules/processors/MappingProcessor.py
+++ ngmUML.backend/ngUML.backend/src/rules/processors/MappingProcessor.py
@@ -2,15 +2,27 @@
 from model.models import Classifier, Property
 
 def getClassifiers():
+    ''' Returns all classfiers '''
     names = list()
     for classifier in Classifier.objects.all():
         names.append(classifier.name)
 
     return names
 
+
 def getProperties():
+    ''' Returns all properties from all classifiers '''
     names = list()
     for property in Property.objects.all():
+        names.append(property.name)
+
+    return names
+
+
+def getPropertiesFromClassifiers(inputClassifier):
+    ''' Returns all properties from the classfier in the parameter. Parameter is the name of the classfier. '''
+    names = list()
+    for property in Property.objects.filter(classifier=Classifier.objects.filter(name=inputClassifier)):
         names.append(property.name)
 
     return names
