632212056e3cda33eb68763c559e920ae69bfa27
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2021-11-15 20:04:00 +0100

    Rename of rule.id to rule.pk and added a debug api call in place of modified

--- ngmUML.backend/ngUML.backend/src/rules/urls.py
+++ ngmUML.backend/ngUML.backend/src/rules/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('add/', views.add),
-    path('remove', views.remove),
-    path('modified/', views.get_modified_rules)
+    path('remove/', views.remove),
+    path('debug/', views.debug),
 ]
